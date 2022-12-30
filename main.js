175adb87b202dfa96e3885abad958047cb3b83bb
PadmareddyUmmannagari <100913606+PadmareddyUmmannagari@users.noreply.github.com>
2022-12-30 10:44:57 +0530

    Update main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+let divContainerEl=document.getElementById('root');
+divContainerEl.classList.add('bg);
+let h1Element=document.createElement('h1);
+h1Element.textContent="Hello World";
+divContainerEl.appendChild(h1Element);
